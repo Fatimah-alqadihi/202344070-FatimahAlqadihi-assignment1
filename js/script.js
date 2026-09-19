d236f8b2fc8288ab3109b33d31e34b58f508d812
Fatimah-alqadihi <s202344070@kfupm.edu.sa>
2026-09-19 15:07:43 +0300

    Added dark mode feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-place holder text
+
+
+const toggleBtn = document.getElementById('theme-toggle');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  // Change button text
+  if (document.body.classList.contains('dark-mode')) {
+    toggleBtn.textContent = "Light Mode";
+  } else {
+    toggleBtn.textContent = "Dark Mode";
+  }
+});
